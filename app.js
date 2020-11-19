387022a1e457e5cfd52e9de91bb0b8170514aebc
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-19 19:04:22 +0000

    changed carousel button position and first try at swipe to move

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function init() {
   const about = document.querySelector('.about')
   const contactLinks = document.querySelectorAll('.contact-section')
   const projectSelectors = document.querySelectorAll('.project-selector')
+  const projects = document.querySelectorAll('.project-show')
 
   // Carousel settings, delay prevents multiple function calls
   let currentProject = 0
@@ -75,6 +76,7 @@ function init() {
       prevBtn.style.opacity = currentProject > 0 ? 1 : 0
       nextBtn.style.opacity = currentProject < 3 ? 1 : 0
     }, titleShowing ? 700 : 200)
+    resizeCarousel()
     // setTimeout(() => projectSelectors.forEach(button => button.style.opacity = 1), titleShowing ? 700 : 200)
   }
 
@@ -161,7 +163,6 @@ function init() {
 
     currentProject = Math.min(Math.max(currentProject + direction, 0), 3)
     
-    const projects = document.querySelectorAll('.project-show')
     projects.forEach((project, i) => adjustCarousel(project, i))
 
     // Hide buttons on either end of carousel
@@ -179,6 +180,8 @@ function init() {
         project.style.transition = 'all 1s'
       }, 100)
     })
+    const links = document.querySelector('.project-links')
+    projectSelectors.forEach(button => button.style.top = links.offsetTop + 'px')
   }
 
   const adjustCarousel = (project, i) => {
@@ -202,6 +205,17 @@ function init() {
     }
   }
 
+  let swipeX
+
+  const beginSwipe = event => {
+    swipeX = event.touches[0].clientX
+  }
+
+  const endSwipe = event => {
+    if (event.changedTouches[0].clientX > swipeX + 30) changeProject(-1)
+    if (event.changedTouches[0].clientX < swipeX - 30) changeProject(1)
+  }
+
   window.addEventListener('mousemove', trackMouseWithGlow)
   monogram.addEventListener('click', loadTitle)
   navItems.forEach(item => item.addEventListener('click', loadSection))
@@ -219,13 +233,20 @@ function init() {
   })
 
   window.addEventListener('keydown', (e) => {
-    const projects = document.querySelector('.projects')
-    if (getComputedStyle(projects).opacity === '0') return
+    const projectSection = document.querySelector('.projects')
+    if (getComputedStyle(projectSection).opacity === '0') return
     if (e.key === 'ArrowLeft') changeProject(-1)
     if (e.key === 'ArrowRight') changeProject(1)
   })
 
   container.addEventListener('scroll', (e) => adjustMask(e.target.scrollTop))
+
+  document.querySelector('.projects').addEventListener('touchstart', beginSwipe)
+  document.querySelector('.projects').addEventListener('touchend', endSwipe)
+
+
+
+  
 }
 
 window.addEventListener('DOMContentLoaded', init)
